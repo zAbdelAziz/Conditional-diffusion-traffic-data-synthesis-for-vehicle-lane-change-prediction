517c3d673f7801834928b86829c859e7e022d2c1
Mohamed <M.abdelaziz@swuppi.com>
2026-02-28 04:01:37 +0100

    Base Dataset [Logic of loading raw/refactored/synthetic dataset files]

---
+++ datasets/base.py
@@ -0,0 +1,128 @@
+from os.path import join
+from pathlib import Path
+
+from numpy import savez_compressed, load
+from pandas import read_csv, DataFrame
+from torch.utils.data import Dataset
+
+from utils.config import Config
+from utils.logger import Logger
+
+
+class BaseDataset(Dataset):
+	def __init__(self, name: str, raw: bool = False):
+		self.name = name
+		self.raw = raw
+		
+		self.cfg = Config()
+		self.log = Logger(self.cfg.runner.name)
+		
+		# Build Paths from config [Raw, Refactored, Synthetic, Standardizers]	
+		self.paths = self._resolve_paths()
+		
+		# Build Refactored [Timeseries] dataset from raw csv
+		# Logic Only [actual refactoring is done by the child class]
+		if self.raw:
+			# If Refactored Dataset is not available we build it
+			if (not self.paths['ref_meta'].exists()) or (not self.paths['ref_npz'].exists()):
+				self._build_refactored()
+			# Otherwise do nothing [Loads the dataset from child class]
+		else:
+			# If Synthetic Data is not available we fall back to refactored raw data [Flip the raw to True]
+			if (not self.paths['syn_meta'].exists()) or (not self.paths['syn_npz'].exists()):
+				self.log.error(f'Synthetic data for {self.name} dataset doesnt exist so falling back to raw data')
+				self.raw = True
+				# If Refactored Dataset is not available we build it
+				if (not self.paths['ref_meta'].exists()) or (not self.paths['ref_npz'].exists()):
+					self._build_refactored()
+				# Otherwise do nothing [Loads the dataset from child class]
+			# Otherwise [meta, npz] exists so we keep the raw as false [Loads the dataset from child class]
+		
+		# Load the Refactored/Synthetic Dataset
+		# # TODO Enable after cleaning the _build_refactored
+		# self.X, self.y, self.meta = self._load_refactored()
+
+	def _build_refactored(self):
+		# Different Datasets Has different Preprocessing and cleaning
+		raise NotImplementedError('Should be implemented by child dataset')
+
+	def _load_refactored(self):
+		# Load the refactored/synthetic datasets from disk
+		# Should be the same logic in all datasets
+		if self.raw:
+			# Load refactored
+			meta = read_csv(self.paths['ref_meta'])
+			data = load(self.paths['ref_npz'], allow_pickle=False)
+		else:
+			# Load Synth
+			meta = read_csv(self.paths['syn_meta'])
+			data = load(self.paths['syn_npz'], allow_pickle=False)
+		
+		# Sanity checks
+		if len(meta) != data['X'].shape[0] or data['X'].shape[0] != data['y'].shape[0]:
+			raise RuntimeError(f'Cache mismatch: meta={len(meta)}, X={data['X'].shape}, y={data['y'].shape}')
+		
+		return data['X'], data['y'], meta
+
+	def _read_raw(self):
+		# Read RAW CSV and clean it
+		raw_path = self.paths["raw"]
+		if not raw_path.exists():
+			raise FileNotFoundError(f"dataset {self.name} raw file not found: {raw_path}")
+
+		# Load selected columns as strings [the values/number of ngsim are in eu format! not sure why! so I just read as str and made a clean method]
+		self.log.info('Reading Raw CSV')
+
+		columns = self.cfg.datasets[self.name].columns
+		if columns is None:
+			df = read_csv(raw_path, dtype=str)
+		else:
+			df = read_csv(raw_path, usecols=columns, dtype=str)
+			# Sanity Check
+			for col in columns:
+				if col not in df.columns:
+					raise KeyError(f"Missing required column '{col}' in {raw_path}. Found: {list(df.columns)}")
+
+		# Clean dataframe [Filters, Dtypes, Drop Nans, Sort]
+		df = self._clean_raw_csv(df=df)
+		return df
+
+	def _clean_raw_csv(self, df: DataFrame):
+		raise NotImplementedError('Should be implemented by child dataset')
+
+	def _resolve_paths(self):
+		# Base Dataset Dir
+		raw_path = Path(join(self.cfg.runner.dirs.raw_datasets, f'{self.name}.csv'))
+
+		# Refactored Paths
+		ref_dir = Path(self.cfg.runner.dirs.refactored_datasets)
+		ref_dir.mkdir(parents=True, exist_ok=True)
+		# Paths
+		ref_meta = Path(join(ref_dir, f'{self.name}.csv'))
+		ref_npz = Path(join(ref_dir, f'{self.name}.npz'))
+		# Raw Standardizer
+		ref_std = Path(join(ref_dir, f'{self.name}.std.npz'))
+		
+		# Synthetic Paths
+		syn_dir = Path(self.cfg.runner.dirs.synth_datasets)
+		syn_dir.mkdir(parents=True, exist_ok=True)
+		# Suffix for Synthetic Dataset [Number of samples]
+		n_syn = self.cfg.runner.dataset.synth_samples
+		syn_suffix = f'-N{int(n_syn)}' if (n_syn is not None) else ''
+		# Paths
+		syn_meta = Path(join(syn_dir, f'{self.name}{syn_suffix}.csv'))
+		syn_npz = Path(join(syn_dir, f'{self.name}{syn_suffix}.npz'))
+		# Synthetic Standardizer
+		derived_std = Path(join(syn_dir, f'{self.name}.derived.std.npz'))
+
+		return {'raw': raw_path,
+				'ref_meta': ref_meta, 'ref_npz': ref_npz,
+				'syn_meta': syn_meta, 'syn_npz': syn_npz,
+				'ref_std': ref_std, 'derived_std': derived_std}
+
+	def save(self):
+		path_prefix = 'ref' if self.raw else 'syn'
+		if self.meta is None or self.X is None or self.y is None:
+			raise RuntimeError(f'Unable to save {self.name} dataset because X or y or meta are None')
+		self.meta.to_csv(self.paths[f'{path_prefix}_meta'], index=False)
+		savez_compressed(self.paths[f'{path_prefix}_npz'], X=self.X, y=self.y)
--- datasets/ngsim/__init__.py
+++ datasets/ngsim/__init__.py
@@ -1,6 +1,21 @@
+from pandas import DataFrame
+
+from datasets.base import BaseDataset
 
 
+class NgsimDataset(BaseDataset):
+	def __init__(self, name: str, raw: bool = False):
+		super().__init__(name=name, raw=raw)
 
-class NgsimDataset:
-	def __init__(self, **kwargs):
-		super().__init__()
+		# # TODO Enable after cleaning the _build_refactored
+		# # X Shape: [n_samples, n_lookback, n_features]
+		# # Numer of Timesteps
+		# self.T = self.X.shape[1]
+		# # Number of features
+		# self.D = self.X.shape[2]
+
+	def _build_refactored(self):
+		self._read_raw()
+
+	def _clean_raw_csv(self, df: DataFrame):
+		return df
