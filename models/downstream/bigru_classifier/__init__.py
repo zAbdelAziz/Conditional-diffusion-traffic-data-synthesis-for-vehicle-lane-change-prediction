e7408b626439da0158bcd64dbf138ee2ca73fb52
Mohamed <M.abdelaziz@swuppi.com>
2026-03-02 06:40:38 +0100

    BiGRU Classifier

--- models/downstream/bigru_classifier/__init__.py
+++ models/downstream/bigru_classifier/__init__.py
@@ -1,6 +1,19 @@
-from torch.nn import Module
+from torch.nn import Module, Sequential, LayerNorm, Linear, GRU
 
 
 class BiGRUClassifierModel(Module):
-    def __init__(self, **kwargs):
+    def __init__(self, input_dim: int, hidden_dim: int = 64, num_layers: int = 2, num_classes: int = 3, dropout: float = 0.1):
         super().__init__()
+        self.gru = GRU(input_size=input_dim, hidden_size=hidden_dim, num_layers=num_layers, bidirectional=True, dropout=dropout,
+                       batch_first=True)
+
+        self.head = Sequential(LayerNorm(hidden_dim * 2), Linear(hidden_dim * 2, num_classes))
+
+    def forward(self, x):
+        # out: [B, T, 2H]
+        out, h = self.gru(x)
+        # [B, 2H]
+        last = out[:, -1, :]
+        # [B, 3]
+        logits = self.head(last)
+        return logits
